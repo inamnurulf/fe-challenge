5e1e8f7153c4a0c4a59f39609034df994dc37507
inamnurul <aam.inam7310@gmail.com>
2023-10-26 03:55:03 +0700

    fix: bounching parallax (deleting dependentcy)

--- src/section/hero.tsx
+++ src/section/hero.tsx
@@ -1,76 +1,113 @@
-"use client";
-import React from "react";
-import Image from "next/image";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import {useSpring} from '@react-spring/web'
+"use client"
+import React, { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
 
 const Hero = () => {
-  return (
-    <section className={"min-h-screen bg-primary"}>
+  const [scrollY, setScrollY] = useState(0);
+  const parallaxRef1 = useRef<HTMLDivElement | null>(null);
+  const parallaxRef2 = useRef<HTMLDivElement | null>(null);
+  const parallaxRef3 = useRef<HTMLDivElement | null>(null);
+  const parallaxRef4 = useRef<HTMLDivElement | null>(null);
+  const parallaxRefDrone = useRef<HTMLDivElement | null>(null);
+  const parallaxRefMoon = useRef<HTMLDivElement | null>(null);
+  const parallaxRefText = useRef<HTMLDivElement | null>(null);
+
+
+  const handleScroll = () => {
+    setScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (
+      parallaxRef1.current &&
+      parallaxRef2.current &&
+      parallaxRef3.current &&
+      parallaxRef4.current &&
+      parallaxRefDrone.current&&
+      parallaxRefMoon.current&&
+      parallaxRefText.current
       
-      <Parallax pages={1.5} className="scrollbar-hide">
-      <ParallaxLayer offset={0.3} speed={-0.3}>
-          <Image
-            className="absolute top-0 w-full"
-            src={"./moon.svg"}
-            width={1440}
-            height={1024}
-            alt={"moon"}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.3} speed={-0.3}>
-          <Image
-            className="absolute bottom-0 w-full"
-            src={"./layer4.svg"}
-            width={1440}
-            height={1024}
-            alt={"city4"}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.3} speed={-0.25}>
-          <Image
-            className="absolute bottom-0 w-full"
-            src={"./layer3.svg"}
-            width={1440}
-            height={1024}
-            alt={"city4"}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.3} speed={-0.2}>
-          <Image
-            className="absolute bottom-0 w-full"
-            src={"./layer2.svg"}
-            width={1440}
-            height={1024}
-            alt={"city4"}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.3} speed={-0.2} className="flex justify-center">
-          <div className="text-xl font-roboto font-black text-white [text-shadow:0px_4px_4px_var(--tw-shadow-color)] shadow-black text-6xl relative top-[30%]" 
+    ) {
+      parallaxRef1.current.style.transform = `translateY(+${scrollY * 0.6}px)`;
+      parallaxRef2.current.style.transform = `translateY(+${scrollY * 0.4}px)`;
+      parallaxRef3.current.style.transform = `translateY(+${scrollY * 0.3}px)`;
+      parallaxRef4.current.style.transform = `translateY(+${scrollY * 0.0}px)`;
+      parallaxRefDrone.current.style.transform = `translateY(+${scrollY * 0.1}px)`;
+      parallaxRefMoon.current.style.transform = `translateY(+${scrollY * 0.7}px)`;
+      parallaxRefText.current.style.transform = `translateY(+${scrollY * 0.1}px)`;
+
+    }
+  }, [scrollY]);
+
+  return (
+    <section className="h-[100vh] relative overflow-hidden ">
+      <div ref={parallaxRefMoon} className="absolute top-0 w-full">
+        <Image
+         className=''
+          src="./moon.svg"
+          width={386}
+          height={386}
+          alt="city4"
+        />
+      </div>
+      <div ref={parallaxRef1} className="absolute bottom-0 w-full">
+        <Image
+         className='w-full min-w-[1000px]'
+          src="./layer4.svg"
+          width={1440}
+          height={1024}
+          alt="city4"
+        />
+      </div>
+      <div ref={parallaxRef2} className="absolute bottom-0 w-full">
+        <Image
+        className='w-full min-w-[1000px]'
+          src="./layer3.svg"
+          width={1440}
+          height={1024}
+          alt="city3"
+        />
+      </div>
+      <div ref={parallaxRef3} className="absolute bottom-0 w-full">
+        <Image
+        className='w-full min-w-[1000px]'
+          src="./layer2.svg"
+          width={1440}
+          height={1024}
+          alt="city2"
+        />
+      </div>
+      <div ref={parallaxRefText} className="absolute w-full h-screen flex justify-center ">
+      <div className="text-xl font-roboto font-black text-white [text-shadow:0px_4px_4px_var(--tw-shadow-color)] shadow-black text-6xl relative top-[30%]" 
           >Front End Developer Challenge</div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.3} speed={-0.15}>
-          <Image
-            className="absolute bottom-[30%] w-full"
-            src={"./drone.svg"}
-            width={1440}
-            height={1024}
-            alt={"drone"}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={0.3} speed={-0.15}>
-          <Image
-            className="absolute bottom-0 w-full"
-            src={"./layer1.svg"}
-            width={1440}
-            height={1024}
-            alt={"city4"}
-          />
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.3} speed={-0.15}>
-          <div className="h-screen w-screen bg-[#001842]"></div>
-        </ParallaxLayer>
-      </Parallax>
+      </div>
+      
+      <div ref={parallaxRef4} className="absolute bottom-0 w-full">
+        <Image
+        className='w-full min-w-[1000px]'
+          src="./layer1.svg"
+          width={1440}
+          height={1024}
+          alt="city1"
+        />
+      </div>
+      <div ref={parallaxRefDrone} className="absolute w-full">
+        <Image
+        className='w-full min-w-[1000px]'
+          src="./drone.svg"
+          width={1440}
+          height={1024}
+          alt="drone"
+        />
+      </div>
     </section>
   );
 };
