9161f886cc3332f2a3d2855e944fb12a5b7e003d
inamnurul <aam.inam7310@gmail.com>
2023-10-29 02:50:12 +0700

    fix: minor styling

--- src/components/scollUpButton.tsx
+++ src/components/scollUpButton.tsx
@@ -33,7 +33,7 @@ const ScrollToTopButton = () => {
       {showButton ? (
         <button
           onClick={scrollToTop}
-          className={` bounce transition-opacity transition-transform fixed z-[100] md:bottom-8 bottom-5 md:right-8 right-5 bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-full transform translate-y-0 duration-300 ease-in-out opacity-100 duration-300 ease-in-out`}
+          className={` animate-bounce transition-opacity transition-transform fixed z-[100] md:bottom-8 bottom-5 md:right-8 right-5 bg-blue-500 hover:bg-blue-600 text-white p-2 rounded-full transform translate-y-0 duration-300 ease-in-out opacity-100 duration-300 ease-in-out`}
         >
           <FaArrowUp />
         </button>
